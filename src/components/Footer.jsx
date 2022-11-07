539d0b1fb6fbd8292a5e7768c02cc4baa6980287
jignesh <85551495+jigneshbhai@users.noreply.github.com>
2022-11-07 12:22:08 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyKIDA</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://github.com/jigneshbhai" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
